c025393380a63d99d82c8f63efd6aa6d0fa43978
Luis Alberto Jaime B. <39748335+luisJaimeB@users.noreply.github.com>
2025-10-31 19:31:41 -0500

    feat: Are installed tailwind and preline.

--- natification/src/main.js
+++ natification/src/main.js
@@ -1,4 +1,8 @@
 import './assets/main.css'
+import "preline/dist/index.js";
+// o si estás usando script tag:
+// <script src="/node_modules/preline/dist/preline.js"></script>
+
 
 import { createApp } from 'vue'
 import App from './App.vue'
